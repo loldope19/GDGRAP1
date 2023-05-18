629fbe4db0a427796ff4b731ec29fe8bf0b00841
Sydrenz Cao <sydrenz_cao@dlsu.edu.ph>
2023-05-18 11:16:32 +0800

    wabbit

--- glfwsetup.cpp
+++ glfwsetup.cpp
@@ -1,11 +1,12 @@
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <math.h>
 
+#define TINYOBJLOADER_IMPLEMENTATION
+#include "tiny_obj_loader.h"
+
 int main(void) 
 {
-    float intAngle = 72;
-    intAngle *= 3.14167 / 180.0f;
-    float angle;
     GLFWwindow* window;
 
     /* Initialize the library */
@@ -13,7 +14,7 @@ int main(void)
         return -1;
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(480, 480, "Sydrenz Cao", NULL, NULL);
+    window = glfwCreateWindow(600, 600, "Sydrenz Cao", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
@@ -22,6 +23,83 @@ int main(void)
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
+    gladLoadGL();
+
+    std::string path = "3D/bunny.obj";
+    std::vector<tinyobj::shape_t> shapes;
+    std::vector<tinyobj::material_t> material;
+    std::string warning, error;
+
+    tinyobj::attrib_t attributes;
+
+    bool success = tinyobj::LoadObj(
+        &attributes,
+        &shapes,
+        &material,
+        &warning,
+        &error,
+        path.c_str()
+    );
+
+    std::vector<GLuint> mesh_indices;
+    for (int i = 0; i < shapes[0].mesh.indices.size(); i++) {
+        mesh_indices.push_back(
+            shapes[0].mesh.indices[i].vertex_index
+            );
+    }
+
+    GLfloat vertices[]{
+        //x    y     z
+        0.5f, 0.5f, 0.f,        // Point 0
+        -0.5f, -0.5f, 0.f,      // Point 1
+        0.5f, -0.5f, 0.f        // Point 2
+    };
+
+    GLfloat indices[]{
+        0, 1, 2
+    };
+
+    GLuint VAO, VBO, EBO;
+    glGenVertexArrays(1, &VAO);     // Generates 1 VAO and outputs GLuint
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
+    // Tells OpenGL we're working on this VAO
+    glBindVertexArray(VAO);
+
+    // Assigns VBO to this VAO
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(
+        GL_ARRAY_BUFFER,    // Type of Buffer
+        sizeof(GL_FLOAT) * attributes.vertices.size(),   // Size in bytes
+        &attributes.vertices[0],           // Array itself
+        GL_STATIC_DRAW      // Static (for now, GL_DYNAMIC_ARRAY if moving)
+    );
+
+    glVertexAttribPointer(
+        0,          // 0 == Position
+        3,          // XYZ
+        GL_FLOAT,    // what array it is
+        GL_FALSE,
+        3 * sizeof(GL_FLOAT),
+        (void*)0
+    );
+
+    // Enables Index 0
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(
+        GL_ELEMENT_ARRAY_BUFFER,
+        sizeof(GLuint) * mesh_indices.size(),
+        mesh_indices.data(),
+        GL_STATIC_DRAW
+    );
+
+    // Tells OpenGL we're done w/ VBO and VAO
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
@@ -29,15 +107,14 @@ int main(void)
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-
-        glBegin(GL_TRIANGLE_FAN);
-        angle = 0.0f;
-        for (int i = 0; i < 5; i++) {
-            glVertex3f(0.5f * cos(angle), 0.5 * sin(angle), 0.f);
-            angle += intAngle;
-        }
-        glEnd();
-
+        glBindVertexArray(VAO);
+        //glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(
+            GL_TRIANGLES,
+            mesh_indices.size(),
+            GL_UNSIGNED_INT,
+            0
+        );
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
@@ -45,6 +122,10 @@ int main(void)
         /* Poll for and process events */
         glfwPollEvents();
     }
+
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
 
     glfwTerminate();
     return 0;
