97d6cf809fa6fb2ddcad35888b00a1be2c8f5b9c
Sydrenz Cao <sydrenz_cao@dlsu.edu.ph>
2023-06-15 23:02:41 +0800

    FPS Camera

--- glfwsetup.cpp
+++ glfwsetup.cpp
@@ -28,74 +28,71 @@ float scale_mod = 0;
 float theta_xmod = 0, theta_ymod = 0;
 float zoom_mod = 0;
 
+bool firstMouse = true;
+float yaw = -90.0f;	// yaw is initialized to -90.0 degrees since a yaw of 0.0 results in a direction vector pointing to the right so we initially rotate a bit to the left.
+float pitch = 0.0f;
+float lastX = 600 / 2.0;
+float lastY = 600 / 2.0;
+
 glm::mat4 identity_matrix = glm::mat4(1.0f);
 
-void Key_Callback(
-    GLFWwindow* window,
-    int key,        // Key Code
-    int scancode,
-    int action,
-    int mod
-) {
-    if (key == GLFW_KEY_D &&
-        action == GLFW_PRESS) {
-        x_mod += 1.0f;              // Movement (X-Axis)
+glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, -5.0f);
+glm::vec3 center = glm::vec3(0, 0, 5.0f);
+glm::vec3 WorldUp = glm::vec3(0, 1.0f, 0);
+
+void processInput(GLFWwindow* window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+
+    float cameraSpeed = 0.005f;
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        cameraPos += cameraSpeed * center;
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        cameraPos -= cameraSpeed * center;
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        cameraPos -= glm::normalize(glm::cross(center, WorldUp)) * cameraSpeed;
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        cameraPos += glm::normalize(glm::cross(center, WorldUp)) * cameraSpeed;
+    if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS) {
+
+    }
+    
+    cameraPos.y = 0.0f;         // Makes it so that the camera does not fly around
+}
+
+void mouse_callback(GLFWwindow* window, double xpos, double ypos)
+{
+    if (firstMouse)
+    {
+        lastX = xpos;
+        lastY = ypos;
+        firstMouse = false;
     }
 
-    if (key == GLFW_KEY_A &&
-        action == GLFW_PRESS) {
-        x_mod -= 1.0f;              // Movement (X-Axis)
-    }
+    float xoffset = xpos - lastX;
+    float yoffset = lastY - ypos;
+    lastX = xpos;
+    lastY = ypos;
 
-    if (key == GLFW_KEY_W &&
-        action == GLFW_PRESS) {
-        y_mod += 1.0f;              // Movement (Y-Axis)
-    }
+    float sensitivity = 0.2f;
+    xoffset *= sensitivity;
+    yoffset *= sensitivity;
 
-    if (key == GLFW_KEY_S &&
-        action == GLFW_PRESS) {
-        y_mod -= 1.0f;              // Movement (Y-Axis)
-    }
+    yaw += xoffset;
+    pitch += yoffset;
 
-    if (key == GLFW_KEY_UP &&
-        action == GLFW_PRESS) {
-        theta_xmod += 2.0f;         // Rotation (X-Axis)
-    }
+    if (pitch > 89.0f)
+        pitch = 89.0f;
+    if (pitch < -89.0f)
+        pitch = -89.0f;
 
-    if (key == GLFW_KEY_DOWN &&
-        action == GLFW_PRESS) {
-        theta_xmod -= 2.0f;         // Rotation (X-Axis)
-    }
-
-    if (key == GLFW_KEY_LEFT &&
-        action == GLFW_PRESS) {
-        theta_ymod += 2.0f;         // Rotation (Y-Axis)
-    }
-
-    if (key == GLFW_KEY_RIGHT &&
-        action == GLFW_PRESS) {
-        theta_ymod -= 2.0f;         // Rotation (Y-Axis)
-    }
-
-    if (key == GLFW_KEY_Q &&
-        action == GLFW_PRESS) {
-        scale_mod += 1.0f;          // Increase Scale
-    }
-
-    if (key == GLFW_KEY_E &&
-        action == GLFW_PRESS) {
-        scale_mod -= 1.0f;          // Decrease Scale
-    }
-
-    if (key == GLFW_KEY_Z &&
-        action == GLFW_PRESS) {
-        zoom_mod += 0.5f;          // Zoom In
-    }
-
-    if (key == GLFW_KEY_X &&
-        action == GLFW_PRESS) {
-        zoom_mod -= 0.5f;          // Zoom Out
-    }
+    glm::vec3 direction;
+    direction.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+    direction.y = sin(glm::radians(pitch));
+    direction.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+    //center = glm::normalize(direction);
+    center = direction;
 }
 
 int main(void) 
@@ -163,7 +160,8 @@ int main(void)
     // Enable Depth Testing
     glEnable(GL_DEPTH_TEST);
 
-    glfwSetKeyCallback(window, Key_Callback);
+    glfwSetCursorPosCallback(window, mouse_callback);
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
     std::fstream vertSrc("Shaders/sample.vert");
     std::stringstream vertBuff;
@@ -206,7 +204,7 @@ int main(void)
         &error,
         path.c_str()
     );
-    /*
+
     GLfloat UV[]{       // UV Data
         0.f, 1.f,
         0.f, 0.f,
@@ -217,19 +215,6 @@ int main(void)
         0.f, 1.f,
         0.f, 0.f
     };
-    */
-
-    GLfloat UV[]{       // UV Data
-        0.f, 10.f,
-        0.f, 0.f,
-        10.f, 10.f,
-        10.f, 0.f,
-        10.f, 10.f,
-        10.f, 0.f,
-        0.f, 10.f,
-        0.f, 0.f
-    };
-
 
     GLint objColorLoc = glGetUniformLocation(shaderProgram, "objColor");
 
@@ -238,24 +223,6 @@ int main(void)
         mesh_indices.push_back(
             shapes[0].mesh.indices[i].vertex_index
             );
-    }
-
-    std::vector<GLfloat> fullVertexData;
-    for (int i = 0; i < shapes[0].mesh.indices.size(); i++) {
-        tinyobj::index_t vData = shapes[0].mesh.indices[i];
-        fullVertexData.push_back(
-            attributes.vertices[vData.vertex_index * 3]         // X-Position
-        );
-        fullVertexData.push_back(
-            attributes.vertices[vData.vertex_index * 3 + 1]
-        );
-        fullVertexData.push_back(
-            attributes.vertices[vData.vertex_index * 3 + 2]
-        );
-
-        fullVertexData.push_back(
-            attributes.texcoords[]
-        )
     }
 
     GLfloat vertices[]{
@@ -331,20 +298,18 @@ int main(void)
     glBindVertexArray(0);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
-    
-
     glViewport(0, 0, width, height);
-    //glUniform3f(objColorLoc, 0.5f, 0.1f, 0.1f);
     glm::mat4 projection = glm::perspective(
         glm::radians(60.f), // FOV
         height / width,   // Aspect Ratio
         0.1f,   // Near
-        50.0f   // Far
+        100.0f   // Far
     );
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
+        processInput(window);
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
@@ -355,17 +320,12 @@ int main(void)
         // Use the texture at 0
         glUniform1i(tex0Address, 0);
 
-        glm::mat4 transformation_matrix = glm::translate(identity_matrix, glm::vec3(x , y , z + zoom_mod));
+        glm::mat4 transformation_matrix = glm::translate(identity_matrix, glm::vec3(x + x_mod , y , z + zoom_mod));
         transformation_matrix = glm::scale(transformation_matrix, glm::vec3(scale_x + scale_mod, scale_y + scale_mod, scale_z + scale_mod));
-        transformation_matrix = glm::rotate(transformation_matrix, glm::radians(theta_xmod += y_mod), glm::normalize(glm::vec3(axis_x, 0, axis_z)));     // Rotation w/ Normalized X-Axis
-        transformation_matrix = glm::rotate(transformation_matrix, glm::radians(theta_ymod += x_mod), glm::normalize(glm::vec3(0, axis_y, axis_z)));     // Rotation w/ Normalized Y-Axis
-       
-        glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, -5.0f);    // Top View
+        transformation_matrix = glm::rotate(transformation_matrix, glm::radians(theta_xmod), glm::normalize(glm::vec3(axis_x, 0, axis_z)));     // Rotation w/ Normalized X-Axis
+        transformation_matrix = glm::rotate(transformation_matrix, glm::radians(theta_ymod), glm::normalize(glm::vec3(0, axis_y, axis_z)));     // Rotation w/ Normalized Y-Axis
 
-        glm::vec3 WorldUp = glm::vec3(0, 1.0f, 0);
-        glm::vec3 center = glm::vec3(0, 1.0f, 0);
-
-        glm::mat4 viewMatrix = glm::lookAt(cameraPos, center, WorldUp);
+        glm::mat4 viewMatrix = glm::lookAt(cameraPos, cameraPos + center, WorldUp);
 
         unsigned int transformLoc = glGetUniformLocation(shaderProgram, "transform");
         glUniformMatrix4fv(transformLoc,                // Address of the transform variable
@@ -384,87 +344,6 @@ int main(void)
             1,                              // How many matrices to assign
             GL_FALSE,                       // Transpose?
             glm::value_ptr(projection));     // Pointer to the matrix
-        /*
-        glUseProgram(shaderProgram);
-
-        glBindVertexArray(VAO);
-        glDrawElements(
-            GL_TRIANGLES,
-            mesh_indices.size(),
-            GL_UNSIGNED_INT,
-            0
-        );
-        
-        glViewport(300, 300, width / 2, height / 2);
-        glUniform3f(objColorLoc, 0.0f, 1.0f, 0.0f);
-        glm::mat4 projection2 = glm::perspective(
-            glm::radians(60.f), // FOV
-            height / width,   // Aspect Ratio
-            0.1f,   // Near
-            50.0f   // Far
-        );
-
-        glm::vec3 cameraPos2 = glm::vec3(-5.0f, 0, 0.0f); // Front View
-        glm::mat4 viewMatrix2 = glm::lookAt(cameraPos2, center, WorldUp);
-        unsigned int transformLoc2 = glGetUniformLocation(shaderProgram, "transform");
-        glUniformMatrix4fv(transformLoc2,                // Address of the transform variable
-            1,                                          // How many matrices to assign
-            GL_FALSE,                                   // Transpose?
-            glm::value_ptr(transformation_matrix));     // Pointer to the matrix
-
-        unsigned int viewLoc2 = glGetUniformLocation(shaderProgram, "view");
-        glUniformMatrix4fv(viewLoc2,
-            1,
-            GL_FALSE,
-            glm::value_ptr(viewMatrix2));
-
-        unsigned int projLoc2 = glGetUniformLocation(shaderProgram, "projection");
-        glUniformMatrix4fv(projLoc2,    // Address of the transform variable
-            1,                              // How many matrices to assign
-            GL_FALSE,                       // Transpose?
-            glm::value_ptr(projection2));     // Pointer to the matrix
-
-        glUseProgram(shaderProgram);
-
-        glBindVertexArray(VAO);
-        glDrawElements(
-            GL_TRIANGLES,
-            mesh_indices.size(),
-            GL_UNSIGNED_INT,
-            0
-        );
-
-        glViewport(150, 0, width / 2, height / 2);
-        glUniform3f(objColorLoc, 0.0f, 0.0f, 1.0f);
-        glm::mat4 projection3 = glm::perspective(
-            glm::radians(60.f), // FOV
-            height / width,   // Aspect Ratio
-            0.1f,   // Near
-            50.0f   // Far
-        );
-
-        glm::vec3 cameraPos3 = glm::vec3(0, 0, -5.0f);     // Side View
-        glm::mat4 viewMatrix3 = glm::lookAt(cameraPos3, center, WorldUp);
-
-        unsigned int transformLoc3 = glGetUniformLocation(shaderProgram, "transform");
-        glUniformMatrix4fv(transformLoc3,                // Address of the transform variable
-            1,                                          // How many matrices to assign
-            GL_FALSE,                                   // Transpose?
-            glm::value_ptr(transformation_matrix));     // Pointer to the matrix
-
-        unsigned int viewLoc3 = glGetUniformLocation(shaderProgram, "view");
-        glUniformMatrix4fv(viewLoc3,
-            1,
-            GL_FALSE,
-            glm::value_ptr(viewMatrix3));
-
-        unsigned int projLoc3 = glGetUniformLocation(shaderProgram, "projection");
-        glUniformMatrix4fv(projLoc3,    // Address of the transform variable
-            1,                              // How many matrices to assign
-            GL_FALSE,                       // Transpose?
-            glm::value_ptr(projection3));     // Pointer to the matrix
-
-        */
 
         // ---------------------------- //
         glUseProgram(shaderProgram);
