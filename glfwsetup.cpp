1469d7d18a55a569daae239b14025ce4c70506e1
Sydrenz Cao <sydrenz_cao@dlsu.edu.ph>
2023-07-27 23:11:46 +0800

    The Yae Transparency

--- glfwsetup.cpp
+++ glfwsetup.cpp
@@ -1,16 +1,3 @@
-/* * * * * * * * * * * * * * * * * * * * * * * * * * * 
- * Quiz 2 - #7 (Sydrenz Cao | GDGRAP1)               * 
- * Completed Items:                                  * 
- * - Center it with the full model visible           * 
- * - Constantly rotate it along the Y axis           * 
- * - Use perspective projection with FOV 90          * 
- * - Use a Direction light with direction (-1,-1,0)  * 
- *   and the color green for all components          * 
- *                                                   * 
- * Incomplete Items:                                 *
- * - Apply any texture / picture on the model        *
- * * * * * * * * * * * * * * * * * * * * * * * * * * */
-
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <math.h>
@@ -31,97 +18,28 @@
 float height = 600.f;
 float width = 600.f;
 
-float x = 0, y = 0, z = 0;      // Centered Model (?) [1]
-float scale_x = 1.f, scale_y = 1.f, scale_z = 1.f;
+// Model Variables
+float x = 0, y = 0, z = 0;
+float scale_x = 2.f, scale_y = 2.f, scale_z = 2.f;
 float axis_x = 1.0f, axis_y = 1.0f, axis_z = 0.0f;
+float theta_xmod = 90, theta_ymod = 0;
 
-float x_mod = 0;
-float y_mod = 0;
-float scale_mod = 0;
-float theta_xmod = 0, theta_ymod = 0;
-float zoom_mod = 0;
-
+// Light Variables
 float ambientStr = 0.1f;
 float specStr = 0.5f;
 float specPhong = 0.5f;
 
-glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, -5.0f);
+// Camera Variables
+glm::vec3 cameraPos = glm::vec3(-5.0f, 0.0f, 0.0f);
 glm::vec3 WorldUp = glm::vec3(0, 1.0f, 0);
 glm::vec3 center = glm::vec3(0, 0.0f, 0);
 
+// OpenGL Light Variables
 glm::mat4 identity_matrix = glm::mat4(1.0f);
-glm::vec3 lightPos = glm::vec3(-1, -1, 0);
-glm::vec3 lightColor = glm::vec3(1, 1, 1);      // Color Green [5]
+glm::vec3 lightPos = glm::vec3(-5.f, 0, 0);
+glm::vec3 lightColor = glm::vec3(1, 1, 1);
 
 glm::vec3 ambientColor = lightColor;
-
-void Key_Callback(
-    GLFWwindow* window,
-    int key,        // Key Code
-    int scancode,
-    int action,
-    int mod
-) {
-    if (key == GLFW_KEY_D &&
-        action == GLFW_PRESS) {
-        x_mod += 1.0f;              // Movement (X-Axis)
-    }
-
-    if (key == GLFW_KEY_A &&
-        action == GLFW_PRESS) {
-        x_mod -= 1.0f;              // Movement (X-Axis)
-    }
-
-    if (key == GLFW_KEY_W &&
-        action == GLFW_PRESS) {
-        y_mod += 1.0f;              // Movement (Y-Axis)
-    }
-
-    if (key == GLFW_KEY_S &&
-        action == GLFW_PRESS) {
-        y_mod -= 1.0f;              // Movement (Y-Axis)
-    }
-
-    if (key == GLFW_KEY_UP &&
-        action == GLFW_PRESS) {
-        theta_xmod += 2.0f;         // Rotation (X-Axis)
-    }
-
-    if (key == GLFW_KEY_DOWN &&
-        action == GLFW_PRESS) {
-        theta_xmod -= 2.0f;         // Rotation (X-Axis)
-    }
-
-    if (key == GLFW_KEY_LEFT &&
-        action == GLFW_PRESS) {
-        theta_ymod += 2.0f;         // Rotation (Y-Axis)
-    }
-
-    if (key == GLFW_KEY_RIGHT &&
-        action == GLFW_PRESS) {
-        theta_ymod -= 2.0f;         // Rotation (Y-Axis)
-    }
-
-    if (key == GLFW_KEY_Q &&
-        action == GLFW_PRESS) {
-        scale_mod += 1.0f;          // Increase Scale
-    }
-
-    if (key == GLFW_KEY_E &&
-        action == GLFW_PRESS) {
-        scale_mod -= 1.0f;          // Decrease Scale
-    }
-
-    if (key == GLFW_KEY_Z &&
-        action == GLFW_PRESS) {
-        zoom_mod += 0.5f;          // Zoom In
-    }
-
-    if (key == GLFW_KEY_X &&
-        action == GLFW_PRESS) {
-        zoom_mod -= 0.5f;          // Zoom Out
-    }
-}
 
 int main(void)
 {
@@ -147,33 +65,43 @@ int main(void)
         img_height,     // Texture Height
         colorChannels;  // No. of Color Channels
 
-    // Fix the flipped texture
+    // Fixes the flipped texture
     stbi_set_flip_vertically_on_load(true);
 
     // Loads the texture and fills out the variables
+    // 1st Texture - Brickwall
     unsigned char* tex_bytes =
-        stbi_load("3d/yae.png",   // Texture Path
+        stbi_load("3d/brickwall.jpg",   // Texture Path
             &img_width,             // Fills out the width
             &img_height,            // Fills out the height
             &colorChannels,         // Fills out the color channel
             0);
 
+    // 2nd Texture - Yae
+    int img_width2, img_height2, colorChannel2;
+    stbi_set_flip_vertically_on_load(true);
+    unsigned char* tex_bytes2 = stbi_load("3D/yae.png", &img_width2, &img_height2, &colorChannel2, 0);
+
+    // 3rd Texture - Brickwall (Normal)
+    int img_width3, img_height3, colorChannel3;
+    stbi_set_flip_vertically_on_load(true);
+    unsigned char* normal_bytes = stbi_load("3D/brickwall_normal.jpg", &img_width3, &img_height3, &colorChannel3, 0);
+
+    // -------- 1st Image Generation -------- //
     // OpenGL reference to the texture
     GLuint texture;
-
     // Generate a reference
     glGenTextures(1, &texture);
     glActiveTexture(GL_TEXTURE0);
     glBindTexture(GL_TEXTURE_2D, texture);
 
-    // Assign the loaded texture to the OpenGL reference
     glTexImage2D(GL_TEXTURE_2D,
         0,                          // Texture 0
-        GL_RGBA,                    // Target color format of the texture
+        GL_RGB,                    // Target color format of the texture
         img_width,                  // Texture width
         img_height,                 // Texture height
         0,
-        GL_RGBA,                    // Color format of the texture
+        GL_RGB,                    // Color format of the texture
         GL_UNSIGNED_BYTE,
         tex_bytes);                 // Loaded texture in bytes
 
@@ -182,11 +110,38 @@ int main(void)
     // Free up the loaded bytes
     stbi_image_free(tex_bytes);
 
+    // -------- 2nd Image Generation -------- //
+
+    GLuint texture2;
+    glGenTextures(1, &texture2);
+    glActiveTexture(GL_TEXTURE1);
+    glBindTexture(GL_TEXTURE_2D, texture2);
+
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, img_width2, img_height2, 0, GL_RGBA, GL_UNSIGNED_BYTE, tex_bytes2);
+    glGenerateMipmap(GL_TEXTURE_2D);
+    stbi_image_free(tex_bytes2);
+
+    // -------- 3rd Image Generation -------- //
+
+    GLuint norm_tex;
+    glGenTextures(1, &norm_tex);
+    glActiveTexture(GL_TEXTURE2);
+    glBindTexture(GL_TEXTURE_2D, norm_tex); 
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
+
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, img_width3, img_height3, 0, GL_RGB, GL_UNSIGNED_BYTE, normal_bytes);
+    glGenerateMipmap(GL_TEXTURE_2D);
+    stbi_image_free(normal_bytes);
+
     // Enable Depth Testing
     glEnable(GL_DEPTH_TEST);
 
-    glfwSetKeyCallback(window, Key_Callback);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
+    // ------- Shader Initialization ------- //
     std::fstream skyboxVertSrc("Shaders/skybox.vert");
     std::stringstream skyboxVertBuff;
     skyboxVertBuff << skyboxVertSrc.rdbuf();
@@ -243,7 +198,9 @@ int main(void)
     glDeleteShader(vertShader);
     glDeleteShader(fragShader);
 
-    std::string path = "3D/djSword.obj";
+    // ------- Model Initialization ------- //
+
+    std::string path = "3D/plane.obj";
     std::vector<tinyobj::shape_t> shapes;
     std::vector<tinyobj::material_t> material;
     std::string warning, error;
@@ -259,27 +216,7 @@ int main(void)
         path.c_str()
     );
 
-    GLfloat UV[]{       // UV Data
-        0.f, 1.f,
-        0.f, 0.f,
-        1.f, 1.f,
-        1.f, 0.f,
-        1.f, 1.f,
-        1.f, 0.f,
-        0.f, 1.f,
-        0.f, 0.f
-    };
-
-    /*
-  7--------6
- /|       /|
-4--------5 |
-| |      | |
-| 3------|-2
-|/       |/
-0--------1
-*/
-//Vertices for the cube
+    //Vertices for the cube
     float skyboxVertices[]{
         -1.f, -1.f, 1.f, //0
         1.f, -1.f, 1.f,  //1
@@ -321,13 +258,71 @@ int main(void)
         "Skybox/rainbow_bk.png"
     };
 
-    GLint objColorLoc = glGetUniformLocation(shaderProgram, "objColor");
-
     std::vector<GLuint> mesh_indices;
     for (int i = 0; i < shapes[0].mesh.indices.size(); i++) {
         mesh_indices.push_back(
             shapes[0].mesh.indices[i].vertex_index
         );
+    }
+
+    std::vector<glm::vec3> tangents;
+    std::vector<glm::vec3> bitangents;
+
+    for (int i = 0; i < shapes[0].mesh.indices.size(); i += 3) {
+        tinyobj::index_t vData1 = shapes[0].mesh.indices[i];
+        tinyobj::index_t vData2 = shapes[0].mesh.indices[i + 1];
+        tinyobj::index_t vData3 = shapes[0].mesh.indices[i + 2];
+
+        glm::vec3 v1 = glm::vec3(
+            attributes.vertices[vData1.vertex_index * 3],
+            attributes.vertices[(vData1.vertex_index * 3) + 1],
+            attributes.vertices[(vData1.vertex_index * 3) + 2]
+        );
+
+        glm::vec3 v2 = glm::vec3(
+            attributes.vertices[vData2.vertex_index * 3],
+            attributes.vertices[(vData2.vertex_index * 3) + 1],
+            attributes.vertices[(vData2.vertex_index * 3) + 2]
+        );
+
+        glm::vec3 v3 = glm::vec3(
+            attributes.vertices[vData3.vertex_index * 3],
+            attributes.vertices[(vData3.vertex_index * 3) + 1],
+            attributes.vertices[(vData3.vertex_index * 3) + 2]
+        );
+
+        glm::vec2 uv1 = glm::vec2(
+            attributes.texcoords[(vData1.texcoord_index * 2)],
+            attributes.texcoords[(vData1.texcoord_index * 2) + 1]
+        );
+
+        glm::vec2 uv2 = glm::vec2(
+            attributes.texcoords[(vData2.texcoord_index * 2)],
+            attributes.texcoords[(vData2.texcoord_index * 2) + 1]
+        );
+
+        glm::vec2 uv3 = glm::vec2(
+            attributes.texcoords[(vData3.texcoord_index * 2)],
+            attributes.texcoords[(vData3.texcoord_index * 2) + 1]
+        );
+
+        glm::vec3 deltaPos1 = v2 - v1;
+        glm::vec3 deltaPos2 = v3 - v1;
+
+        glm::vec2 deltaUV1 = uv2 - uv1;
+        glm::vec2 deltaUV2 = uv3 - uv1;
+
+        float r = 1.0f / ((deltaUV1.x * deltaUV2.y) - (deltaUV1.y * deltaUV2.x));
+        glm::vec3 tangent = (deltaPos1 * deltaUV2.y - deltaPos2 * deltaUV1.y) * r;
+        glm::vec3 bitangent = (deltaPos2 * deltaUV1.x - deltaPos1 * deltaUV2.x) * r;
+
+        tangents.push_back(tangent);
+        tangents.push_back(tangent);
+        tangents.push_back(tangent);
+
+        bitangents.push_back(bitangent);
+        bitangents.push_back(bitangent);
+        bitangents.push_back(bitangent);
     }
 
     std::vector<GLfloat> fullVertexData;
@@ -365,8 +360,33 @@ int main(void)
         fullVertexData.push_back(
             attributes.texcoords[(vData.texcoord_index * 2) + 1]
         );
+
+        fullVertexData.push_back(
+            tangents[i].x
+        );
+
+        fullVertexData.push_back(
+            tangents[i].y
+        );
+
+        fullVertexData.push_back(
+            tangents[i].z
+        );
+
+        fullVertexData.push_back(
+            bitangents[i].x
+        );
+
+        fullVertexData.push_back(
+            bitangents[i].y
+        );
+
+        fullVertexData.push_back(
+            bitangents[i].z
+        );
     }
 
+    // ------- Skybox Initialization ------- //
     unsigned int skyboxTex;
     glGenTextures(1, &skyboxTex);
     glBindTexture(GL_TEXTURE_CUBE_MAP, skyboxTex);
@@ -391,17 +411,6 @@ int main(void)
 
     stbi_set_flip_vertically_on_load(true);
 
-    GLfloat vertices[]{
-        //x    y     z
-        0.5f, 0.5f, 0.f,        // Point 0
-        -0.5f, -0.5f, 0.f,      // Point 1
-        0.5f, -0.5f, 0.f        // Point 2
-    };
-
-    GLfloat indices[]{
-        0, 1, 2
-    };
-
     unsigned int skyboxVAO, skyboxVBO, skyboxEBO;
     glGenVertexArrays(1, &skyboxVAO);
     glGenBuffers(1, &skyboxVBO);
@@ -413,7 +422,7 @@ int main(void)
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, skyboxEBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GL_INT) * 36, &skyboxIndices, GL_STATIC_DRAW);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int) * 36, &skyboxIndices, GL_STATIC_DRAW);
 
     glEnableVertexAttribArray(0);
 
@@ -430,41 +439,26 @@ int main(void)
         GL_DYNAMIC_DRAW
     );
 
-    glVertexAttribPointer(
-        0,          // 0 == Position
-        3,          // XYZ
-        GL_FLOAT,    // what array it is
-        GL_FALSE,
-        8 * sizeof(GL_FLOAT),
-        (void*)0
-    );
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 14 * sizeof(GL_FLOAT), (void*)0);
 
     GLintptr normalPtr = 3 * sizeof(float);
-    glVertexAttribPointer(
-        1,
-        3,
-        GL_FLOAT,
-        GL_FALSE,
-        8 * sizeof(float),
-        (void*)normalPtr
-    );
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 14 * sizeof(float), (void*)normalPtr);
 
     GLintptr uvPtr = 6 * sizeof(float);
-    glVertexAttribPointer(
-        2,
-        2,
-        GL_FLOAT,
-        GL_FALSE,
-        8 * sizeof(float),
-        (void*)uvPtr
-    );
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 14 * sizeof(float), (void*)uvPtr);
 
-    // Enables Index 0
+    GLintptr tangentPtr = 8 * sizeof(float);
+    glVertexAttribPointer(3, 3, GL_FLOAT, GL_FALSE, 14 * sizeof(float), (void*)tangentPtr);
+
+    GLintptr bitangentPtr = 11 * sizeof(float);
+    glVertexAttribPointer(4, 3, GL_FLOAT, GL_FALSE, 14 * sizeof(float), (void*)bitangentPtr);
+
+    // Enables Indexes 0 - 4
     glEnableVertexAttribArray(0);
     glEnableVertexAttribArray(1);
-
-    // Enable 2 for our UV / Tex coords
     glEnableVertexAttribArray(2);
+    glEnableVertexAttribArray(3);
+    glEnableVertexAttribArray(4);
 
     // Tells OpenGL we're done w/ VBO and VAO
     glBindBuffer(GL_ARRAY_BUFFER, 0);
@@ -473,7 +467,7 @@ int main(void)
 
     glViewport(0, 0, width, height);
     glm::mat4 projection = glm::perspective(
-        glm::radians(90.f), // FOV 90 [4]
+        glm::radians(90.f),
         height / width, 
         0.1f, 
         50.0f 
@@ -485,15 +479,10 @@ int main(void)
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+        // Start rendering the Skybox
         glDepthMask(GL_FALSE);
         glDepthFunc(GL_LEQUAL);
         glUseProgram(skyboxShaderProg);
-
-        glm::mat4 transformation_matrix = glm::translate(identity_matrix, glm::vec3(x, y, z + zoom_mod));
-        transformation_matrix = glm::scale(transformation_matrix, glm::vec3(scale_x + scale_mod, scale_y + scale_mod, scale_z + scale_mod));
-        transformation_matrix = glm::rotate(transformation_matrix, glm::radians(theta_xmod += y_mod), glm::normalize(glm::vec3(axis_x, 0, axis_z)));     // Rotation w/ Normalized X-Axis
-        // Y-Axis Rotation [3]
-        transformation_matrix = glm::rotate(transformation_matrix, glm::radians(theta_ymod += 0.2f), glm::normalize(glm::vec3(0, axis_y, axis_z)));     // Rotation w/ Normalized Y-Axis
 
         glm::mat4 viewMatrix = glm::lookAt(cameraPos, center, WorldUp);
         
@@ -507,8 +496,22 @@ int main(void)
         unsigned int skyboxProjLoc = glGetUniformLocation(skyboxShaderProg, "projection");
         glUniformMatrix4fv(skyboxProjLoc, 1, GL_FALSE, glm::value_ptr(projection));
 
+        glBindVertexArray(skyboxVAO);
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_CUBE_MAP, skyboxTex);
+        glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
+        glDepthMask(GL_TRUE);
+        glDepthFunc(GL_LESS);
+        
         // ---------------------------- //
+        // ------- Model Rendering ------- //
+        glUseProgram(shaderProgram);
 
+        glm::mat4 transformation_matrix = glm::translate(identity_matrix, glm::vec3(x, y, z));
+        transformation_matrix = glm::scale(transformation_matrix, glm::vec3(scale_x, scale_y, scale_z));
+        transformation_matrix = glm::rotate(transformation_matrix, glm::radians(theta_xmod), glm::normalize(glm::vec3(axis_x, 0, axis_z)));     // Rotation w/ Normalized X-Axis
+        transformation_matrix = glm::rotate(transformation_matrix, glm::radians(theta_ymod += -0.02f), glm::normalize(glm::vec3(0, axis_y, axis_z)));     // Rotation w/ Normalized Y-Axis
+        
         unsigned int transformLoc = glGetUniformLocation(shaderProgram, "transform");
         glUniformMatrix4fv(transformLoc,                // Address of the transform variable
             1,                                          // How many matrices to assign
@@ -520,7 +523,7 @@ int main(void)
             1,
             GL_FALSE,
             glm::value_ptr(viewMatrix));
-
+        
         unsigned int projLoc = glGetUniformLocation(shaderProgram, "projection");
         glUniformMatrix4fv(projLoc,    // Address of the transform variable
             1,                              // How many matrices to assign
@@ -528,21 +531,7 @@ int main(void)
             glm::value_ptr(projection));     // Pointer to the matrix
 
         // ---------------------------- //
-               
-        glBindVertexArray(skyboxVAO);
-        glActiveTexture(GL_TEXTURE0);
-        glBindTexture(GL_TEXTURE_CUBE_MAP, skyboxTex);
-        //glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
-        glDepthMask(GL_TRUE);
-        glDepthFunc(GL_LESS);
         
-        // Get the location of tex 0 in the fragment shader
-        GLuint tex0Address = glGetUniformLocation(shaderProgram, "tex0");
-        // Tell OpenGL to use the texture
-        glBindTexture(GL_TEXTURE_2D, texture);
-        // Use the texture at 0
-        glUniform1i(tex0Address, 0);
-
         GLuint lightAddress = glGetUniformLocation(shaderProgram, "lightPos");
         glUniform3fv(lightAddress, 1, glm::value_ptr(lightPos));
 
@@ -562,12 +551,26 @@ int main(void)
         glUniform1f(specStrAddress, specStr);
 
         GLuint specPhongAddress = glGetUniformLocation(shaderProgram, "specPhong");
-        glUniform1f(specStrAddress, specPhong);
+        glUniform1f(specPhongAddress, specPhong);
 
-        glUseProgram(shaderProgram);
+        glActiveTexture(GL_TEXTURE0);       // Renders Brickwall
+        glBindTexture(GL_TEXTURE_2D, texture);
+        GLuint tex0Address = glGetUniformLocation(shaderProgram, "tex0");
+        glUniform1i(tex0Address, 0);
+
+        glActiveTexture(GL_TEXTURE1);       // Renders Yae (ultimately not sure as to the order between Yae and Brickwall (Normal))
+        glBindTexture(GL_TEXTURE_2D, texture2);
+        GLuint tex1Address = glGetUniformLocation(shaderProgram, "tex1");
+        glUniform1i(tex1Address, 1);
+
+        glActiveTexture(GL_TEXTURE2);       // Renders Brickwall (Normal)
+        glBindTexture(GL_TEXTURE_2D, norm_tex);
+        GLuint tex2Loc = glGetUniformLocation(shaderProgram, "norm_tex");
+        glUniform1i(tex2Loc, 2);
 
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, fullVertexData.size() / 8);
+        glDrawArrays(GL_TRIANGLES, 0, fullVertexData.size() / 14);
+
         // ---------------------------- //
 
         /* Swap front and back buffers */
--- x64/Debug/Shaders/sample.frag
+++ x64/Debug/Shaders/sample.frag
@@ -1,6 +1,8 @@
 #version 330 core
 // Texture to be passed
 uniform sampler2D tex0;
+uniform sampler2D tex1;
+uniform sampler2D norm_tex;
 
 uniform vec3 lightPos;
 uniform vec3 lightColor;
@@ -17,6 +19,8 @@ in vec2 texCoord;
 in vec3 normCoord;
 in vec3 fragPos;
 
+in mat3 TBN;
+
 uniform vec3 objColor; // Object Color
 out vec4 FragColor; // Color pixel
 
@@ -29,31 +33,29 @@ void main(){
 	}
 
 	// FragColor = vec4(objColor, 1.0f);
+	vec4 texColor0 = texture(tex0, texCoord);			// Brickwall Texture
+	vec4 texColor1 = texture(tex1, texCoord);			// Yae Texture
+	vec4 texColor = mix(texColor0, texColor1, 0.5);		// Both textures blended at semi-transparency (0.5 as per direction)
 
 	// Assigns the texture color using the function
-	FragColor = texture(tex0, texCoord);
+	FragColor = texColor;
+	
+	vec3 normal = texture(norm_tex, texCoord).rgb;
+	normal = normalize(normal * 2.0 - 1.0);
+	normal = normalize(TBN * normal);
 
-	vec3 normal = normalize(normCoord);
-	vec3 lightDir = normalize(-lightPos);		// Main Implementation of Direction Light (basically normalizing the (negative) light Position)
-
-	// Gets the distance between the light and the position of the fragment
-	// float fDistance = length(lightPos - fragPos);
-	// Intensity of the light with regards to the distance of the fragment to the light source
-	// Equation is 1 / Distance^2
-	// float fIntensity = 1 / (fDistance * fDistance);
+	vec3 lightDir = normalize(-lightPos);		// Utilized Direction Light instead of Point Light for a brighter model
 
 	float diff = max(dot(normal, lightDir), 0.0);
-	// vec3 diffuse = diff * lightColor * fIntensity;		// Light intensity factored on the diffuse component of the object
 	vec3 diffuse = diff * lightColor;
 
 	vec3 ambientCol = ambientColor * ambientStr;
 
 	vec3 viewDir = normalize(cameraPos - fragPos);
 	vec3 reflectDir = reflect(-lightDir, normal);
-
+	
 	float spec = pow(max(dot(reflectDir, viewDir), 0.1), specPhong);
-	//vec3 specColor = spec * specStr * lightColor * fIntensity;		// Light intensity factored on the specular component of the object, 
 	vec3 specColor = spec * specStr * lightColor;
 
-	FragColor = vec4(specColor + ambientCol + diffuse, 1.0) * texture(tex0, texCoord);
+	FragColor = vec4(specColor + ambientCol + diffuse, 1.0) * FragColor;
 }
